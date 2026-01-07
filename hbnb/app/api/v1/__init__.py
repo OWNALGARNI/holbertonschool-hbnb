0c6002c540704ca7eebc563d975a55a13c038e03
OWNALGARNI <s.xooxs@hotmail.com>
2026-01-07 16:43:10 +0300

    Delete hbnb directory

--- hbnb/app/api/v1/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.api.v1.users import users_ns
-
-namespaces = [users_ns]
--- hbnb/app/facade/hbnb_facade.py
+++
@@ -1,7 +0,0 @@
-from app.persistence.repository import InMemoryRepository
-
-
-class HBnBFacade:
-    def __init__(self) -> None:
-        self.repo = InMemoryRepository
-    
